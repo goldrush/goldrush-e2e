a0a1815b1957a5dbcde972ba6ab17b382395142a
Tatsunori TAMAKI <qtamaki@gmail.com>
2013-10-02 09:49:27 +0900

    初期化

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GoldrushE2e::Application.config.secret_token = '546c8e99718e1f155d870663264e759cbc908a0c8bd083b49aec591abfb677572979cfb6b080cdeed7536c6d868fe34c0532d6e1f946d7628e9d20b1c81eb5bb'
